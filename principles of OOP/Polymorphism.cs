32a1a7eb3053ceccb53851f0d18360c09f72a125
Maksim <makskupriyanoff@yandex.ru>
2024-11-10 08:36:36 +0300

    [OOP] Решена третья задача с полиморфизмом

---
+++ principles of OOP/Polymorphism.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace principles_of_OOP
+{
+    internal class Polymorphism
+    {
+        public class Animal
+        {
+            public virtual void MakeSound()
+            {
+                Console.WriteLine("Некое животное издает некий звук.");
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Гав! Гав!");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Мяу! Мяу!");
+            }
+        }
+
+        //public class Program
+        //{
+        //    public static void Main()
+        //    {
+        //        Animal myDog = new Dog();
+        //        Animal myCat = new Cat();
+        //        myDog.MakeSound();  // Output: Woof! Woof!
+        //        myCat.MakeSound();  // Output: Meow! Meow!
+        //    }
+        //}
+    }
+}
--- principles of OOP/Program.cs
+++ principles of OOP/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static principles_of_OOP.Encapsulation;
+using static principles_of_OOP.Inheritance;
+using static principles_of_OOP.Polymorphism;
+Console.WriteLine("===Encapsulation===");
+BankAccount bankAccount = new BankAccount(1000);
+Console.WriteLine("Начальный баланс: " + bankAccount.GetBalance().ToString());
+bankAccount.Deposit(500);
+bankAccount.Withdraw(900);
+
+Console.WriteLine("===Inheritance===");
+Vehicle vehicle = new Vehicle("KAMAZ", "model X");
+vehicle.DisplayInfo();
+Car car = new Car("Lada", "Priora", 4);
+car.DisplayInfo();
+Bicycle bicycle = new Bicycle("Stels", "21 speeds", true);
+bicycle.DisplayInfo();
+
+Console.WriteLine("===Polymorphism===");
+Animal myDog = new Dog();
+Animal myCat = new Cat();
+myDog.MakeSound();  // Output: Woof! Woof!
+myCat.MakeSound();  // Output: Meow! Meow!
